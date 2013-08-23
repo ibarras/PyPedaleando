e5907c125bfbcc9976ec360e61a6db114a610d19
Martin Ibarra C <mic@xolos.com.mx>
2013-08-23 10:31:43 -0700

    Model Evento
    Cambios de FileField a ImageField para validar que sean imagenes
    correctas.
    Se necesita la libreria PIL se puede instalar con (pi) easy_install PIL

--- evento/models.py
+++ evento/models.py
@@ -1,22 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 # Create your models here.
 
 class Evento(models.Model):
+    usuario     = models.ForeignKey(User,)
     nombre   = models.CharField(max_length=200)
     fecha    = models.DateField('date published')
-    imagen   = models.FileField(max_length=200, upload_to = 'eventos')
+    imagen   = models.ImageField(max_length=200, upload_to = 'eventos')
     descripcion = models.TextField()
-    usuario     = models.ForeignKey(User)
     
     def __str__(self):
         return self.nombre
 
 class Foto(models.Model):
     evento = models.ForeignKey(Evento)
-    foto = models.FileField(max_length=100, upload_to = 'eventos')
+    foto = models.ImageField(max_length=100, upload_to = 'eventos')
     
     def __str__(self):
         return self.foto
