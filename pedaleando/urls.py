7b080f5dc0e749b6d3257dff5e3d4d491890f745
Angel Rodriguez <zerosoul13@gmail.com>
2013-08-27 01:43:29 +0000

    Agregando Foundation CSS, templates (base.html e index.html), configuracion de settings.py, cambio en gitignore. El template base ya contiene una barra de contenido funcional

--- pedaleando/urls.py
+++ pedaleando/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -13,6 +14,15 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
+     url(r'^$', include('evento.urls')),
      url(r'^admin/', include(admin.site.urls)),
-     url(r'^evento/', include('evento.urls')),
+     url(r'^evento/$', include('evento.urls')),
 )
+
+#Setting para servir fotos 
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media_files/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+            #(r'^media_files/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT#, 'show_indexes':True#}),
+)
+
