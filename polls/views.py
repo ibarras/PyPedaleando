92dbe084e7681c8306fac6e44b89aa541a4458f8
Angel Rodriguez <zerosoul13@gmail.com>
2013-08-28 04:29:25 +0000

    Varios cambios interesantes

--- polls/views.py
+++ polls/views.py
@@ -1,1 +1,13 @@
+from django.shortcuts import render
+from polls.models import Poll, Choice
+
 # Create your views here.
+
+"""
+Mostramos todas las encuestas
+"""
+def pollsMain(request):
+    template = "encuestas.html"
+    encuestas_listado = Poll.objects.all().order_by("-pub_date")
+    return render(request, template, locals())
+
